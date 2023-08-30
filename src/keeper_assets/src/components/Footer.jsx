a6f6603b1650f0634091bfd388ee49f5ca4ea25d
IvyZayn <kakapon713@outlook.com>
2023-08-30 21:08:58 +0800

    Build the Header.jsx and Footer.jsx.

--- src/keeper_assets/src/components/Footer.jsx
+++ src/keeper_assets/src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 
 function Footer() {
-
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/keeper_assets/src/components/Header.jsx
+++ src/keeper_assets/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 
 function Header() {
-
+  return (
+    <header>
+      <h1>Keeper</h1>
+    </header>
+  );
 }
 
 export default Header;
