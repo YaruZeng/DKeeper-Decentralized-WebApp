36b6c108ff92d4afa2c43882cc493a01e325377c
IvyZayn <kakapon713@outlook.com>
2023-08-30 21:19:55 +0800

    Add logic to control states of the CreateArea.

--- src/keeper_assets/src/components/CreateArea.jsx
+++ src/keeper_assets/src/components/CreateArea.jsx
@@ -1,7 +1,50 @@
 import React, { useState } from "react";
 
 function CreateArea() {
+  const [newInput, setNewInput] = useState({ // control the states of the input window
+    title: "",
+    content: "",
+  });
 
+  function handleChange(event) { // triggered when the inputs detect changes
+    const { name, value } = event.target; // get the input name and value
+    setNewInput((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value, // only change the input which detected a change and got a new value
+      };
+    });
+  }
+
+  function submitNote(event) { // triggered when the add button is clicked
+    event.preventDefault(); // prevent the automatic page reloading of the form
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={newInput.title}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          onChange={handleChange}
+          value={newInput.content}
+          rows="3"
+        />
+        <button
+          type="submit"
+          onClick={submitNote}
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateArea;
