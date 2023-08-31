2f0c8e10fa57c2f100ab96ed82bcfc7feea01da6
IvyZayn <kakapon713@outlook.com>
2023-08-31 16:30:11 +0800

    Add logic of deleting notes and making notes stable on backend.

--- src/keeper/main.mo
+++ src/keeper/main.mo
@@ -8,7 +8,7 @@ actor Keeper {
     content: Text;
   };
 
-  var notes: List.List<Note> = List.nil<Note>(); // create a list object containing Note type with null as initial
+  stable var notes: List.List<Note> = List.nil<Note>(); // create a list object containing Note type with null as initial
 
   public func createNote(titleText: Text, contentText: Text) {
     // add a new note to database
@@ -24,5 +24,12 @@ actor Keeper {
   public query func readNotes(): async [Note] { // return an array of Note which is more efficient in JavaScript
     // query for notes from database
     return List.toArray(notes);
+  };
+
+  public func removeNote(id: Nat) { 
+    // delete a note from notes List
+    let listFront = List.take(notes, id);
+    let listBack = List.drop(notes, id + 1);
+    notes := List.append(listFront, listBack);
   };
 }
--- src/keeper_assets/src/components/App.jsx
+++ src/keeper_assets/src/components/App.jsx
@@ -13,21 +13,23 @@ function App() {
     setNotes((prevItems) => {
       keeper.createNote(newItem.title, newItem.content); // create a new note on backend
       return [newItem, ...prevItems]; // add the newItem to the start of previous array
+      // remain the frontend logic so that it can be rendered immidiately 
     });
   }
 
   useEffect(() => { // triggered once the App func is triggered
     console.log("useEffect is triggered");
-    fetchData();
+    fetchData(); // read notes from database
   }, []); // stop once the func is rendered once
 
   async function fetchData(){ // get notes from database
-    const notesArray = await keeper.readNotes();
-    setNotes(notesArray);
+    const notesArray = await keeper.readNotes(); // read notes from database
+    setNotes(notesArray); // update the frontend state of notes
   };
 
   function DeletNote(id) {
     // triggered when the delete button is clicked
+    keeper.removeNote(id);
     setNotes((prevItems) => {
       return prevItems.filter((item, index) => { // delete the item with id passed in
         return index !== id;
